778d212f0ea73fc9238da190c0d63d8eebf75b8d
Dipak Khade <dipakhade214@gmail.com>
2025-11-20 17:01:47 +0530

    added api routes

---
+++ src/app/api/contact-us/route.ts
@@ -0,0 +1,19 @@
+import { getContactMessages } from "@/db/db-queries"
+import { type NextRequest, NextResponse } from "next/server"
+
+
+export async function GET(request: NextRequest) {
+  try {
+    const searchParams = request.nextUrl.searchParams
+    const page = Number.parseInt(searchParams.get("page") || "1")
+    const limit = Number.parseInt(searchParams.get("limit") || "10")
+    const search = searchParams.get("search") || ""
+    const sortBy = searchParams.get("sortBy") || "_id"
+    const sortOrder = Number.parseInt(searchParams.get("sortOrder") || "1")
+
+    const data = await getContactMessages(page, limit, search, sortBy, sortOrder)
+    return NextResponse.json(data)
+  } catch (error) {
+    return NextResponse.json({ error: "Failed to fetch contact messages" }, { status: 500 })
+  }
+}
