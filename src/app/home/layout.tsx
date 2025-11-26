7d9716fdcfa3f8cfb10fb3cb9a569d9c0c83d8e3
Dipak Khade <dipakhade214@gmail.com>
2025-11-26 20:56:16 +0530

    sidebar init

--- src/app/home/layout.tsx
+++ src/app/home/layout.tsx
@@ -1,15 +1,15 @@
 import { Header } from "@/components/header";
-import { Sidebar } from "@/components/sidebar";
+import { Sidebar } from "@/components/sidebar_old";
+import { SidebarProvider, SidebarTrigger } from "@/components/ui/sidebar";
 import { ReactNode } from "react";
 
-
 export default function HomeLayout({children}:{children: ReactNode}) {
-    return <main>
-        <Sidebar />
-      <div className="ml-0 lg:ml-64 h-screen overflow-auto">
-        <Header />
-        {children}
-      </div>
+    return <SidebarProvider>
+    {/* <AppSidebar /> */}
+    <main>
+      <SidebarTrigger />
+      {children}
     </main>
+  </SidebarProvider>
 
 }
