74d21fe40052f83ea9e62ffdf587ddd2aef35173
Dipak Khade <dipakhade214@gmail.com>
2025-12-23 17:19:29 +0530

    db coll mapper init

---
+++ src/lib/collection_mapper.ts
@@ -0,0 +1,10 @@
+
+export const collection_mapper = {
+    "User": "users",
+    "Course": "courses",
+    "Enrollment": "enrollments",
+    "Payment": "payments",
+    "ContactUs": "contacts",
+    "WpGroupMembers": "members",
+    "AssessmentResult": "results"
+}
