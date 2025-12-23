a1f3fe85612d5b540ec579cad483fcb69ab48516
Dipak Khade <dipakhade214@gmail.com>
2025-12-23 17:20:30 +0530

    wrapperd child by apollo provider

--- src/app/home/layout.tsx
+++ src/app/home/layout.tsx
@@ -1,6 +1,10 @@
 import { AppSidebar } from "@/components/app-sidebar";
 import { ReactNode } from "react";
+import { ApolloProvider } from "@apollo/client/react";
+import { apolloClient } from "@/graphql_confs/apollo-client";
 
 export default function HomeLayout({children}:{children: ReactNode}) {
-    return <AppSidebar children={children} />
+    return <ApolloProvider client={apolloClient}>
+      <AppSidebar children={children} />
+    </ApolloProvider>   
 }
