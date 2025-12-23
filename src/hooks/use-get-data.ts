85b829dffa561b36dfbd8346269fa7a30f02057e
Dipak Khade <dipakhade214@gmail.com>
2025-12-23 18:21:16 +0530

    custom hook to get data nd return with id

---
+++ src/hooks/use-get-data.ts
@@ -0,0 +1,21 @@
+import { TypedDocumentNode } from "@apollo/client";
+import { useQuery } from "@apollo/client/react";
+
+export const useGetData = (query: TypedDocumentNode) => {
+    const { loading, error, data } = useQuery(query);
+    let key = "";
+    let updated_data = [];
+    if (data) {
+        key = Object.keys(data)[0];
+        updated_data = data[key].map((x: any) => {
+            let id = x._id;
+            return {
+                ...x,
+                id,
+            };
+        });
+    }
+    console.log(updated_data)
+    
+    return { loading, error, data: updated_data };
+}
