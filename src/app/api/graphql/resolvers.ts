21aa0b5701159000586ea311429a77883561e535
Dipak Khade <dipakhade214@gmail.com>
2025-12-25 22:36:15 +0530

    query for contact us users data

--- src/app/api/graphql/resolvers.ts
+++ src/app/api/graphql/resolvers.ts
@@ -7,6 +7,10 @@ export const resolvers = {
         users: async (_: any, __: any, ___: any) => {  
             const data = (await get_db_connection()).collection(collection_mapper.user_collection).find({}).sort({"name": 1}).toArray();
             return data;    
-        }
+        },
+        contactUs: async (_: any, __: any, ___: any) => {  
+            const data = (await get_db_connection()).collection(collection_mapper.contact_us_collection).find({}).sort({"firstName": 1}).toArray();
+            return data;    
+        },
     },
 };
